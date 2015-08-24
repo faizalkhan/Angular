eae05990bdac96305d12d19b3f47afd825138675
faizalkhan <faizalkhan.s@gmail.com>
2015-08-24 09:38:39 +0530

    user-from and list added

--- personController.js
+++ personController.js
@@ -31,7 +31,7 @@ app.controller('formnames', function($scope, $http)
 	});
 	
 })
-<<<<<<< HEAD
+
 app.controller('counters', function($scope)
 {
 	$scope.count = 0;
@@ -48,7 +48,7 @@ app.controller('goodness', function($scope)
 		$scope.care = !$scope.care;
 	}
 });
-=======
+
 
 app.controller('formcontrol', function($scope)
 {
@@ -67,4 +67,65 @@ app.controller('validationcontrol', function($scope)
 	$scope.user = "imran";
 	$scope.email= "imran.deo@gmail.com";
 })
->>>>>>> ea4d63fbbfd97b894f891b841d129d0680d92a94
+
+app.controller('apis', function($scope)
+{
+	//$scope.x1 ="JOHN";
+	//$scope.x2= angular.lowercase($scope.x1);
+	
+	$scope.x1 ="John";
+	$scope.x2= angular.isNumber($scope.x1);
+	
+});
+
+
+app.controller('stylehero', function($scope) {
+$scope.fName = '';
+$scope.lName = '';
+$scope.passw1 = '';
+$scope.passw2 = '';
+$scope.users = [
+{id:1, fName:'Hege', lName:"Pege" },
+{id:2, fName:'Kim',  lName:"Pim" },
+{id:3, fName:'Sal',  lName:"Smith" },
+{id:4, fName:'Jack', lName:"Jones" },
+{id:5, fName:'John', lName:"Doe" },
+{id:6, fName:'dPeter',lName:"Pan" }
+];
+$scope.edit = true;
+$scope.error = false;
+$scope.incomplete = false;
+
+$scope.editUser = function(id) {
+  if (id == 'new') {
+    $scope.edit = true;
+    $scope.incomplete = true;
+    $scope.fName = '';
+    $scope.lName = '';
+    } else {
+    $scope.edit = false;
+    $scope.fName = $scope.users[id-1].fName;
+    $scope.lName = $scope.users[id-1].lName;
+  }
+};
+
+$scope.$watch('passw1',function() {$scope.test();});
+$scope.$watch('passw2',function() {$scope.test();});
+$scope.$watch('fName', function() {$scope.test();});
+$scope.$watch('lName', function() {$scope.test();});
+
+$scope.test = function() {
+  if ($scope.passw1 !== $scope.passw2) {
+    $scope.error = true;
+    } else {
+    $scope.error = false;
+  }
+  $scope.incomplete = false;
+  if ($scope.edit && (!$scope.fName.length ||
+  !$scope.lName.length ||
+  !$scope.passw1.length || !$scope.passw2.length)) {
+     $scope.incomplete = true;
+  }
+};
+
+});
